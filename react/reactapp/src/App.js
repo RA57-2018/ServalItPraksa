3799e859f5f9af17a8cdf1fcd039deb79510cff3
RA57-2018 <bugarski.ra57.2018@uns.ac.rs>
2022-06-29 09:57:11 +0200

    showing numbers in squares

--- react/reactapp/src/App.js
+++ react/reactapp/src/App.js
@@ -1,6 +1,7 @@
-import React, {useState} from "react";
+import React, {useState, useReducer} from "react";
 import './App.css';
 import Square from "./components/Square";
+
 
 const App = () => {
 
--- react/reactapp/src/components/Square.js
+++ react/reactapp/src/components/Square.js
@@ -1,9 +1,18 @@
-import React from "react";
+import React,{useState, useReducer} from "react";
 
 function Square(){
 
+    const [counter, setCounter] = useState(1);
+
+    let iterator = () => {
+        setCounter(counter + 1);
+    };
+
     return (
            <div className="Square">
+            <div className="counter">
+                {counter}
+            </div>
            </div> 
         
     );
